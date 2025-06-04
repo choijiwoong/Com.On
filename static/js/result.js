939f9237c68f60bc0641a0e4e60befee02645da9
gogogi313 <gogogi313@naver.com>
2025-06-04 21:27:46 +0900

    feat. result에 GTM빠져있는거 발견하여 추가 시도

--- static/js/result.js
+++ static/js/result.js
@@ -1,3 +1,15 @@
+// Google Tag Manager 삽입
+(function (w, d, s, l, i) {
+  w[l] = w[l] || [];
+  w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+  var f = d.getElementsByTagName(s)[0],
+    j = d.createElement(s),
+    dl = l != 'dataLayer' ? '&l=' + l : '';
+  j.async = true;
+  j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+  f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-MZHQSKG5');
+
 // URL 파라미터에서 'query' 값을 추출
 const params = new URLSearchParams(window.location.search);
 const query = params.get("query");
